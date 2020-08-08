6cc219605498412b2648f1ea15a362891167fa76
Parker Johansen <johansen.parker@gmail.com>
2020-08-08 13:04:40 -0600

    Completd identification

--- src/Machine.hs
+++ src/Machine.hs
@@ -2,5 +2,11 @@ module Machine
     ( getValue
     ) where
 
-getValue :: Float -> Float -> Float -> Integer -> Integer
-getValue weight diameter thickness reeds = 5
+getValue :: Float -> Float -> Float -> Integer -> Maybe Integer
+getValue 5.000  21.21    1.95      0     = Just 5   --Nickel
+getValue 2.268  17.91    1.35      118   = Just 10  --Dime
+getValue 5.670  24.26    1.75      119   = Just 25  -- Quarter
+getValue weight diameter thickness reeds = Nothing
+--getValue 2.500  19.05    1.52      0     = Just 1   --Penny
+--getValue 11.340 30.61    2.15      150   = Just 50  --Half Dollar
+--getValue 8.1    26.49    2.00      0     = Just 100 --Dollar
--- test/MachineSpec.hs
+++ test/MachineSpec.hs
@@ -8,12 +8,17 @@ import Test.Validity
 spec :: Spec
 spec = do
   describe "when identifying a coin" $ do
-    context "and the coin is a nickel" $ do
-      identificationTest 5 5.0 21.21 1.95 0
-      identificationTest 10 2.268 17.91 1.35 0
+      identificationTest "nickel" (Just 5) 5.0 21.21 1.95 0
+      identificationTest "dime" (Just 10) 2.268 17.91 1.35 118
+      identificationTest "quarter" (Just 25) 5.670 24.26 1.75 119
+      identificationTest "penny" Nothing 2.500 19.05 1.52 0
+      identificationTest "half dollar" Nothing 11.340 30.61 2.15 150
+      identificationTest "dollar" Nothing 8.1 26.49 2.00 0
+      identificationTest "fraud" Nothing 5.0 20.26 1.75 119
 
 
-identificationTest expected weight diameter thickness reeds = do
-  it ("should return value of " ++ show expected) $ do
-    let actual = getValue weight diameter thickness reeds
-    actual `shouldBe` expected
+identificationTest coin expected weight diameter thickness reeds = do
+  context ("and the coin is a " ++ coin) $ do
+    it ("should return value of " ++ show expected) $ do
+      let actual = getValue weight diameter thickness reeds
+      actual `shouldBe` expected
