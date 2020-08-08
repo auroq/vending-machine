93d71e8133039f81789d129b7e60e454d5ce4bff
Parker Johansen <johansen.parker@gmail.com>
2020-08-08 10:00:53 -0600

    Add more failing tests

--- test/MachineSpec.hs
+++ test/MachineSpec.hs
@@ -5,12 +5,15 @@ import Test.Hspec
 import Test.Validity
 
 
-
 spec :: Spec
 spec = do
   describe "when identifying a coin" $ do
     context "and the coin is a nickel" $ do
-      it "should return value of 5" $ do
-        let expected = 5
-        let actual = getValue 5.0 21.21 1.95 0
-        actual `shouldBe` expected
+      identificationTest 5 5.0 21.21 1.95 0
+      identificationTest 10 2.268 17.91 1.35 0
+
+
+identificationTest expected weight diameter thickness reeds = do
+  it ("should return value of " ++ show expected) $ do
+    let actual = getValue weight diameter thickness reeds
+    actual `shouldBe` expected
