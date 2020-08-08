165b584e06a1d16d0299b9d1783a005becbe42b1
Parker Johansen <johansen.parker@gmail.com>
2020-08-08 13:19:46 -0600

    Refactor to use coin data

--- src/Machine.hs
+++ src/Machine.hs
@@ -1,12 +1,16 @@
 module Machine
     ( getValue
+    , Coin (..)
     ) where
 
-getValue :: Float -> Float -> Float -> Integer -> Maybe Integer
-getValue 5.000  21.21    1.95      0     = Just 5   --Nickel
-getValue 2.268  17.91    1.35      118   = Just 10  --Dime
-getValue 5.670  24.26    1.75      119   = Just 25  -- Quarter
-getValue weight diameter thickness reeds = Nothing
---getValue 2.500  19.05    1.52      0     = Just 1   --Penny
---getValue 11.340 30.61    2.15      150   = Just 50  --Half Dollar
---getValue 8.1    26.49    2.00      0     = Just 100 --Dollar
+
+data Coin = Coin { weight :: Float, diameter :: Float, thickness :: Float, reeds :: Integer } deriving (Show, Eq)
+
+getValue :: Coin -> Maybe Integer
+getValue (Coin 5.000  21.21 1.95 0)     = Just 5   --Nickel
+getValue (Coin 2.268  17.91 1.35 118)   = Just 10  --Dime
+getValue (Coin 5.670  24.26 1.75 119)   = Just 25  -- Quarter
+getValue _                              = Nothing
+--getValue (Coin 2.500  19.05 1.52 0)     = Just 1   --Penny
+--getValue (Coin 11.340 30.61 2.15 150)   = Just 50  --Half Dollar
+--getValue (Coin 8.1    26.49 2.00 0)     = Just 100 --Dollar
--- test/MachineSpec.hs
+++ test/MachineSpec.hs
@@ -4,21 +4,29 @@ import Machine
 import Test.Hspec
 import Test.Validity
 
+nickel = Coin 5.0 21.21 1.95 0
+dime = Coin 2.268 17.91 1.35 118
+quarter = Coin 5.670 24.26 1.75 119
+penny = Coin 2.500 19.05 1.52 0
+halfDollar = Coin 11.340 30.61 2.15 150
+dollar = Coin 8.1 26.49 2.00 0
+fraud = Coin 5.0 20.26 1.75 119
 
 spec :: Spec
 spec = do
   describe "when identifying a coin" $ do
-      identificationTest "nickel" (Just 5) 5.0 21.21 1.95 0
-      identificationTest "dime" (Just 10) 2.268 17.91 1.35 118
-      identificationTest "quarter" (Just 25) 5.670 24.26 1.75 119
-      identificationTest "penny" Nothing 2.500 19.05 1.52 0
-      identificationTest "half dollar" Nothing 11.340 30.61 2.15 150
-      identificationTest "dollar" Nothing 8.1 26.49 2.00 0
-      identificationTest "fraud" Nothing 5.0 20.26 1.75 119
+      identificationTest "nickel" (Just 5) nickel
+      identificationTest "dime" (Just 10) dime
+      identificationTest "quarter" (Just 25) quarter
+      identificationTest "penny" Nothing penny
+      identificationTest "half dollar" Nothing halfDollar
+      identificationTest "dollar" Nothing dollar
+      identificationTest "fraud" Nothing fraud
 
 
-identificationTest coin expected weight diameter thickness reeds = do
-  context ("and the coin is a " ++ coin) $ do
+identificationTest :: String -> Maybe Integer -> Coin -> SpecWith ()
+identificationTest coinName expected coin = do
+  context ("and the coin is a " ++ coinName) $ do
     it ("should return value of " ++ show expected) $ do
-      let actual = getValue weight diameter thickness reeds
+      let actual = getValue coin
       actual `shouldBe` expected
