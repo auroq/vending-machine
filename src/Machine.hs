326c43cdaac9b25c08cc9aa71117eec572f75a43
Parker Johansen <johansen.parker@gmail.com>
2020-08-08 14:22:54 -0600

    Add coin acceptance and rejecting

--- src/Machine.hs
+++ src/Machine.hs
@@ -1,6 +1,7 @@
 module Machine
     ( getValue
     , Coin (..)
+    , acceptCoins
     ) where
 
 
@@ -14,3 +15,13 @@ getValue _                              = Nothing
 --getValue (Coin 2.500  19.05 1.52 0)     = Just 1   --Penny
 --getValue (Coin 11.340 30.61 2.15 150)   = Just 50  --Half Dollar
 --getValue (Coin 8.1    26.49 2.00 0)     = Just 100 --Dollar
+
+acceptCoins :: [Coin] -> (Integer, [Coin])
+acceptCoins = foldl acceptCoin (0, [])
+
+acceptCoin :: (Integer, [Coin]) -> Coin -> (Integer, [Coin])
+acceptCoin (total, rejected) coin =
+  let maybeVal = getValue coin
+  in case maybeVal of
+    Just val -> (total + val, rejected)
+    Nothing  -> (total, coin:rejected)
--- test/MachineSpec.hs
+++ test/MachineSpec.hs
@@ -15,18 +15,54 @@ fraud = Coin 5.0 20.26 1.75 119
 spec :: Spec
 spec = do
   describe "when identifying a coin" $ do
-      identificationTest "nickel" (Just 5) nickel
-      identificationTest "dime" (Just 10) dime
-      identificationTest "quarter" (Just 25) quarter
-      identificationTest "penny" Nothing penny
-      identificationTest "half dollar" Nothing halfDollar
-      identificationTest "dollar" Nothing dollar
-      identificationTest "fraud" Nothing fraud
+    identificationTest "nickel" (Just 5) nickel
+    identificationTest "dime" (Just 10) dime
+    identificationTest "quarter" (Just 25) quarter
+    identificationTest "penny" Nothing penny
+    identificationTest "half dollar" Nothing halfDollar
+    identificationTest "dollar" Nothing dollar
+    identificationTest "fraud" Nothing fraud
+  describe "when accepting coins" $ do
+    context "and one valid coin" $ do
+      let (total, returned) = acceptCoins [nickel]
+      let expectedTotal = 5
+      it "should total correctly" $
+        total `shouldBe` expectedTotal
+      it "should return no coins" $
+        returned `shouldBe` []
+    context "and one invalid coin" $ do
+      let (total, returned) = acceptCoins [fraud]
+      let expectedTotal = 0
+      it "should total correctly" $
+        total `shouldBe` expectedTotal
+      it "should return no the coin" $
+        returned `shouldBe` [fraud]
+    context "and many valid coins" $ do
+      let (total, returned) = acceptCoins [nickel, dime, quarter, quarter]
+      let expectedTotal = 65
+      it "should total correctly" $
+        total `shouldBe` expectedTotal
+      it "should return no coins" $
+        returned `shouldBe` []
+    context "and many invalid coins" $ do
+      let (total, returned) = acceptCoins [fraud, fraud, dollar, halfDollar, penny, fraud]
+      let expectedTotal = 0
+      it "should total correctly" $
+        total `shouldBe` expectedTotal
+      it "should return no coins" $
+        returned `shouldBe` [fraud, penny, halfDollar, dollar, fraud, fraud]
+    context "and many mixed coins" $ do
+      let (total, returned) = acceptCoins [penny, nickel, dime, quarter, halfDollar, dollar, fraud]
+      let expectedTotal = 40
+      it "should total correctly" $
+        total `shouldBe` expectedTotal
+      it "should return no invalid coins" $
+        returned `shouldBe` [fraud, dollar, halfDollar, penny]
 
 
 identificationTest :: String -> Maybe Integer -> Coin -> SpecWith ()
-identificationTest coinName expected coin = do
-  context ("and the coin is a " ++ coinName) $ do
+identificationTest coinName expected coin =
+  context ("and the coin is a " ++ coinName) $
     it ("should return value of " ++ show expected) $ do
       let actual = getValue coin
       actual `shouldBe` expected
